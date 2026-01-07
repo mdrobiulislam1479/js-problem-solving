4f7596b0eee5dc8a69027c1bb7ee249f4d52f7db
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-07 23:02:42 +0600

    solved problem 1

---
+++ problem1.js
@@ -0,0 +1,13 @@
+// Problem 1: Reverse a String
+// Write a function that takes a string and returns it reversed.
+
+// Example:
+
+// Input: "hello"
+// Output: "olleh"
+
+let mainInput = "hello";
+
+let reverseOutput = mainInput.split("").reverse().join("");
+
+console.log(reverseOutput);
