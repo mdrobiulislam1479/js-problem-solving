f1047ae18c1bb6940effb8bc1cdc3aaffd2fec0a
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-07 23:48:00 +0600

    solved problem 4

---
+++ problem4.js
@@ -0,0 +1,13 @@
+// Problem 4: Find the Maximum Number
+// Write a function that takes an array of numbers and returns the largest number.
+
+// Example:
+
+// Input: [5, 1, 9, 3]
+// Output: 9
+
+let numbers = [5, 1, 9, 3];
+
+let maxNumber = Math.max(...numbers);
+
+console.log(maxNumber);
