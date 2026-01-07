5466fdcb749346124d70ef831d190732e03c3a99
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-08 00:13:12 +0600

    solved problem 6

---
+++ problem6.js
@@ -0,0 +1,14 @@
+// Problem 6: Sum of All Numbers in an Array
+// Write a function that returns the sum of all numbers in an array.
+
+// Example:
+
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+let numbers = [1, 2, 3, 4];
+let sum = 0;
+
+numbers.map((num) => (sum += num));
+
+console.log(sum);
